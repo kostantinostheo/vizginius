4d2cc4a3273b0d3c2e6ac08c06106f59603ed217
George Ganias <geoganias@gmail.com>
2024-07-08 15:53:27 +0300

    Fix tooltips, range and stuff

--- src/components/LineChartFiltered/index.js
+++ src/components/LineChartFiltered/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Chart } from 'react-google-charts';
 import './index.css'
 
@@ -32,6 +32,7 @@ export function LineChartFiltered() {
             gridlines: {
                 color: '#111827', // Make vertical grid lines invinsible
             },
+            format: '####'
           },
           
           // Vertical axis
@@ -53,6 +54,8 @@ export function LineChartFiltered() {
             gridlines: {
                 color: '#363c45', // Make horizontal grid lines transparent
             },
+            minValue: '-0.1',
+            maxValue: '1.1'
             
           },
           
@@ -74,61 +77,62 @@ export function LineChartFiltered() {
                 3: { color: '#1c91c0' },
             },
     };
-    const exampleData = [
-        ["Year", "Births", "GDP Per Capita", "Unemployment Rate", "Median Age", "Refugee Population"],
-        ["1980", 148134, 5894, 0, 337248, 3750 ],
-        ["1981", 140953, 5380, 0, 337687, 3990 ],
-        ["1982", 137275, 5579, 0, 338522, 3950 ],
-        ["1983", 132608, 5020, 0, 339584, 4090 ],
-        ["1984", 125724, 4853, 0, 340845, 4320 ],
-        ["1985", 116481, 4814, 0, 342183, 4360 ],
-        ["1986", 112810, 5657, 0, 343593, 3280 ],
-        ["1987", 106392, 6565, 0, 345050, 4630 ],
-        ["1988", 107505, 7598, 0, 346617, 8262 ],
-        ["1989", 101657, 7847, 0, 348336, 7490 ],
-        ["1990", 102229, 9600, 0, 350139, 8488 ],
-        ["1991", 102620, 10188, 757, 352041, 8989 ],
-        ["1992", 104081, 11176, 893, 354020, 8456 ],
-        ["1993", 101799, 10402, 1018, 356113, 7859 ],
-        ["1994", 103763, 11091, 981, 358295, 7773 ],
-        ["1995", 101495, 12959, 1012, 360479, 4428 ],
-        ["1996", 100718, 13749, 1040, 362730, 5780 ],
-        ["1997", 102038, 13428, 1047, 365101 , 5520],
-        ["1998", 100894, 13472, 1223, 367590 , 6145],
-        ["1999", 100643, 13250, 1214, 370166 , 6283],
-        ["2000", 103274, 12073, 1134, 372861 , 6653],
-        ["2001", 102282, 12549, 1076, 375674 , 6927],
-        ["2002", 103569, 14178, 1035, 378626 , 2778],
-        ["2003", 104420, 18518, 984, 381677 , 2762],
-        ["2004", 105655, 21995, 1063, 384831 , 2479],
-        ["2005", 107545, 22560, 1007, 388077, 2394 ],
-        ["2006", 112042, 24822, 891, 391382, 2292 ],
-        ["2007", 111926, 28864, 835, 394739, 2228 ],
-        ["2008", 118302, 32128, 766, 398130, 2169 ],
-        ["2009", 117933, 29829, 955, 401567, 1700 ],
-        ["2010", 114766, 26717, 1272, 405037, 1443 ],
-        ["2011", 106428, 25484, 1797, 408634, 1566 ],
-        ["2012", 100371, 21913, 2473, 412327, 2090 ],
-        ["2013", 94134, 21788, 2769, 416135, 3468 ],
-        ["2014", 92149, 21617, 2671, 420091, 10292 ],
-        ["2015", 91877, 18084, 2498, 423922, 24831 ],
-        ["2016", 92898, 17924, 2351, 427716, 46411 ],
-        ["2017", 88553, 18582, 2141, 431508, 38988 ],
-        ["2018", 86440, 19757, 1918, 435434, 61446 ],
-        ["2019", 83756, 19144, 1704, 439489, 80454 ],
-        ["2020", 84764, 17617, 1590, 443513, 103101 ],
-        ["2021", 85346, 20311, 1466, 447398, 119650 ],
-        ["2022", 76095, 20867, 1243, 451012, 160761 ]
-    ];
 
-    const [data, setData] = useState(exampleData);
+    const exampleData = [
+      ["Year", "Births", { type: 'string', role: 'tooltip' }, "GDP Per Capita", { role: 'tooltip', type: 'string' }, "Unemployment Rate", { role: 'tooltip', type: 'string' }, "Emigration Rate", { role: 'tooltip', type: 'string'}, "Inflation Rate", { role: 'tooltip', type: 'string' }],
+    ["1980", 148134, '', 5894, '', 2.86, '', 0, '', 19.239, ''],
+    ["1981", 140953, '', 5380, '', 4.04, '', 0, '', 21.612, ''],
+    ["1982", 137275, '', 5579, '', 5.88, '', 0, '', 27.213, ''],
+    ["1983", 132608, '', 5020, '', 7.8, '', 0, '', 20.612, ''],
+    ["1984", 125724, '', 4853, '', 8.09, '', 0, '', 21.918, ''],
+    ["1985", 116481, '', 4814, '', 7.8, '', 0, '', 19.026, ''],
+    ["1986", 112810, '', 5657, '', 7.3, '', 0, '', 18.882, ''],
+    ["1987", 106392, '', 6565, '', 7.33, '', 0, '', 15.253, ''],
+    ["1988", 107505, '', 7598, '', 7.77, '', 0, '', 16.680, ''],
+    ["1989", 101657, '', 7847, '', 7.5, '', 0, '', 14.499, ''],
+    ["1990", 102229, '', 9600, '', 6.98, '', 0, '', 20.691, ''],
+    ["1991", 102620, '', 10188, '', 7.57, '', 0, '', 19.788, ''],
+    ["1992", 104081, '', 11176, '', 8.93, '', 0, '', 14.799, ''],
+    ["1993", 101799, '', 10402, '', 10.18, '', 0, '', 14.429, ''],
+    ["1994", 103763, '', 11091, '', 9.81, '', 0, '', 11.182, ''],
+    ["1995", 101495, '', 12959, '', 10.12, '', 0, '', 9.792, ''],
+    ["1996", 100718, '', 13749, '', 10.40, '', 0, '', 7.636, ''],
+    ["1997", 102038, '', 13428, '', 10.47, '', 0, '', 6.553, ''],
+    ["1998", 100894, '', 13472, '', 12.23, '', 60.119, '', 5.104, ''],
+    ["1999", 100643, '', 13250, '', 12.14, '', 54.175, '', 3.623, ''],
+    ["2000", 103274, '', 12073, '', 11.34, '', 46.993, '', 1.593, ''],
+    ["2001", 102282, '', 12549, '', 10.76, '', 45.909, '', 3.475, ''],
+    ["2002", 103569, '', 14178, '', 10.35, '', 39.378, '', 3.349, ''],
+    ["2003", 104420, '', 18518, '', 9.84, '', 37.433, '', 3.454, ''],
+    ["2004", 105655, '', 21995, '', 10.63, '', 38.041, '', 3.063, ''],
+    ["2005", 107545, '', 22560, '', 10.07, '', 38.583, '', 2.240, ''],
+    ["2006", 112042, '', 24822, '', 8.91, '', 38.368, '', 3.495, ''],
+    ["2007", 111926, '', 28864, '', 8.35, '', 40.400, '', 3.423, ''],
+    ["2008", 118302, '', 32128, '', 7.66, '', 43.044, '', 4.345, ''],
+    ["2009", 117933, '', 29829, '', 9.55, '', 43.686, '', 2.570, ''],
+    ["2010", 114766, '', 26717, '', 12.72, '', 62.041, '', -0.177, ''],
+    ["2011", 106428, '', 25484, '', 17.97, '', 92.404, '', 0.959, ''],
+    ["2012", 100371, '', 21913, '', 24.73, '', 124.694, '', -0.275, ''],
+    ["2013", 94134, '', 21788, '', 27.69, '', 117.094, '', -2.046, ''],
+    ["2014", 92149, '', 21617, '', 26.71, '', 106.804, '', -1.939, ''],
+    ["2015", 91877, '', 18084, '', 24.98, '', 109.351, '', -0.294, ''],
+    ["2016", 92898, '', 17924, '', 23.51, '', 106.535, '', -0.579, ''],
+    ["2017", 88553, '', 18582, '', 21.41, '', 103.327, '', 0.285, ''],
+    ["2018", 86440, '', 19757, '', 19.18, '', 103.049, '', -0.165, ''],
+    ["2019", 83756, '', 19144, '', 17.04, '', 95.020, '', 0.227, ''],
+    ["2020", 84764, '', 17617, '', 15.90, '', 77.837, '', -0.752, ''],
+    ["2021", 85346, '', 20311, '', 14.66, '', 79.596, '', 1.485, ''],
+    ["2022", 76095, '', 20867, '', 12.43, '', 80.307, '', 7.847, '']
+  ];
+
+    const data = exampleData;
     const [selectedMetric, setSelectedMetric] = useState('GDP Per Capita');
 
     const metricDescriptions = {
-        'GDP Per Capita': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Justo laoreet sit amet cursus sit amet dictum. Ac tortor vitae purus faucibus ornare suspendisse sed nisi lacus. Cras fermentum odio eu feugiat pretium nibh ipsum consequat. Nunc sed augue lacus viverra vitae.',
-        'Unemployment Rate': 'According to N.Apergis (https://doi.org/10.1016/j.jpolmod.2005.01.001)   Eget nulla facilisi etiam dignissim diam quis enim lobortis. Pretium vulputate sapien nec sagittis. Cras adipiscing enim eu turpis egestas pretium aenean pharetra. Sit amet aliquam id diam maecenas ultricies mi. Rhoncus mattis rhoncus urna neque viverra justo nec. Enim neque volutpat ac tincidunt vitae semper quis.',
-        'Median Age' : 'The Refugee Crisis in 2005 even though we cannot claim that has affected the Birth rates in Greece is associated with the economic crisis. High refugees numbers are associated with lower wages, higher housing prices, and higher GDP. (https://www.brookings.edu/articles/10-economic-characteristics-of-refugee-arrivals-and-returns/) ',
-        'Refugee Population' : 'The Refugee Crisis in 2005 even though we cannot claim that has affected the Birth rates in Greece is associated with the economic crisis. High refugees numbers are associated with lower wages, higher housing prices, and higher GDP. (https://www.brookings.edu/articles/10-economic-characteristics-of-refugee-arrivals-and-returns/) '
+        'GDP Per Capita': 'During periods of economic growth, rising GDP per capita can lead to an increase in birth rates. Economic improvement often brings better living conditions, improved healthcare, and increased optimism about the future, encouraging families to have more children. This phenomenon can be seen in some emerging economies where economic prosperity instills confidence and resources necessary to support larger families. As GDP declines families are not confident about having children and as a result birth rates decline.',
+        'Unemployment Rate': 'The unemployment rate and birth rates often exhibit an inverse relationship. Higher unemployment rates can lead to economic insecurity and reduced household income, discouraging families from having children. Conversely, lower unemployment rates typically correlate with greater economic stability, increased disposable income, and a more favorable environment for family planning and child-rearing. This pattern underscores how economic conditions directly impact fertility decisions within societies.',
+        'Emigration Rate' : 'Emigration rates can influence birth rates in complex ways. High emigration rates may lead to demographic shifts, reducing the population of childbearing age and potentially lowering birth rates due to a smaller reproductive pool. Conversely, low emigration rates can contribute to a stable population structure conducive to higher birth rates as families feel more secure about their future within the country. No data was found before 1998.',
+        'Inflation Rate' : 'The inflation rate can impact birth rates through economic uncertainty and financial stability. High inflation rates often lead to increased living costs, making it more challenging for families to afford child-rearing expenses, thus potentially lowering birth rates. Conversely, stable or lower inflation rates may contribute to economic confidence and affordability, potentially supporting higher birth rates as families feel more secure about their financial future.'
     };
   
     const normalizeData = (data) => {
@@ -136,22 +140,33 @@ export function LineChartFiltered() {
 
         // Find min and max for each metric
         data[0].forEach((metric, i) => {
-            if (i > 0) { // Skip 'Year' column
+            if (i % 2 === 1) { // Skip 'Year' & Tooltips columns
                 const values = data.slice(1).map(row => row[i]);
                 minMax[metric] = { min: Math.min(...values), max: Math.max(...values) };
             }
-            });
+        });
 
             // Normalize values
-            return data.map((row, rowIndex) => {
-            if (rowIndex === 0) return row; // Header row
+        const returnValue = data.map((row, rowIndex) => {
+            if (rowIndex === 0){
+                return row; // Header row
+            }
             return row.map((cell, cellIndex) => {
-                if (cellIndex === 0) return cell; // Year column
-                const metric = data[0][cellIndex];
-                const { min, max } = minMax[metric];
-                return (cell - min) / (max - min);
+                if (cellIndex === 0){ // Year column
+                    return cell;
+                }
+                else if(cellIndex % 2 === 1){ // if its the actual value
+                    const metric = data[0][cellIndex];
+                    const { min, max } = minMax[metric];
+                    return (cell - min) / (max - min);
+                }
+                else { //tooltips
+                    return 'Year: '+data[rowIndex][0]+'\n'+data[0][cellIndex-1]+': '+data[rowIndex][cellIndex-1];
+                }
             });
         });
+        console.log(returnValue)
+        return returnValue;
     };
 
     const normalizedData = normalizeData(data);
@@ -166,18 +181,23 @@ export function LineChartFiltered() {
         return row;
       }
       return row.map((cell, cellIndex) => {
-        if (cellIndex === 0 || cellIndex === 1) {
+        if (cellIndex === 0 || cellIndex === 1 || cellIndex === 2){
           return cell;
         }
-        const metricName = normalizedData[0][cellIndex];
-        return metricName === selectedMetric ? cell : Number.NaN; 
+        else if( cellIndex % 2 === 1){ // if its the actual value
+            const metricName = normalizedData[0][cellIndex];
+            return metricName === selectedMetric ? cell : Number.NaN; 
+        }
+        else{ //if its the tooltip
+            const metricName = normalizedData[0][cellIndex - 1];
+            return metricName === selectedMetric ? cell : null; 
+        }
       });
     });
   
   
     return (
         <div className="flex justify-center items-center p-3">
-            
             <div className='pl-5' style={{marginRight: '-60px'}}>
                 <label className='text-left' style={{ display: 'block', marginBottom: '10px'}}>
                     <input
@@ -198,34 +218,33 @@ export function LineChartFiltered() {
                 <label className='text-left' style={{ display: 'block', marginBottom: '10px'}}>
                     <input
                     type="radio"
-                    checked={selectedMetric === 'Median Age'}
-                    onChange={() => toggleMetric('Median Age')}
+                    checked={selectedMetric === 'Emigration Rate'}
+                    onChange={() => toggleMetric('Emigration Rate')}
                     />
-                    <span style={{marginLeft: '4px'}}>Median Age</span>
+                    <span style={{marginLeft: '4px'}}>Emigration Rate</span>
                 </label>
                 <label className='text-left' style={{ display: 'block', marginBottom: '10px'}}>
                     <input
                     type="radio"
-                    checked={selectedMetric === 'Refugee Population'}
-                    onChange={() => toggleMetric('Refugee Population')}
+                    checked={selectedMetric === 'Inflation Rate'}
+                    onChange={() => toggleMetric('Inflation Rate')}
                     />
-                    <span style={{marginLeft: '4px'}}>Refugee Population</span>
+                    <span style={{marginLeft: '4px'}}>Inflation Rate</span>
                 </label>
             </div>
             <div style={{width: '60%'}}>
                 <Chart
                     chartType="LineChart"
                     width="100%"
-                    height="400px"
+                    height="450px"
                     data={filteredData}
                     options={options}
                 />
-
             </div>
 
-            <p className={'text-justify p-3 rightToLeftAnimate' } style={{width: '30%'}}>
-                    {metricDescriptions[selectedMetric]}
-                </p>
+            <p className={'text-justify p-3 rightToLeftAnimate'} style={{width: '30%'}}>
+                {metricDescriptions[selectedMetric]}
+            </p>
       </div>
     );
 };
